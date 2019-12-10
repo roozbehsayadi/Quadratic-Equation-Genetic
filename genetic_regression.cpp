19c56ac4e0cd0119286d1087fff002600c5b800a
Roozbeh Sayadi <roozbehsayadi@gmail.com>
2019-12-11 01:45:53 +0330

    Crossover causes some strange memory exceptions

--- genetic_regression.cpp
+++ genetic_regression.cpp
@@ -10,7 +10,14 @@ const int DOTS_COUNT = 1000;
 const int POPULATION = 100;
 const int GENERATION_COUNT = 100;
 
+const double MUTATION_PROBABILITY = 0.1;
+
 namespace utilities { 
+
+	union doubleBinary { 
+		double dValue;
+		long long iValue;
+	};
 
 	double doubleRand( double fMin, double fMax ) { 
 		double f = (double) rand() / RAND_MAX;
@@ -19,9 +26,9 @@ namespace utilities {
 
 	std::string decimalToBinaryString( long long a ) { 
 		std::string binary = "";
-		int mask = 1;
+		long long mask = 1;
 		for ( int i = 0; i < 64; i++ ) { 
-			if ( ( mask & a ) >= 1 ) 
+			if ( ( mask & a ) >= 1 )
 				binary = "1" + binary;
 			else
 				binary = "0" + binary;
@@ -30,12 +37,20 @@ namespace utilities {
 		return binary;
 	}
 
-}
+	std::string doubleToBinaryString( double in ) { 
+		doubleBinary temp;
+		temp.dValue = in;
+		return utilities::decimalToBinaryString( temp.iValue );
+	}
 
-union doubleBinary { 
-	double dValue;
-	long long iValue;
-};
+	double binaryStringToDouble( std::string s ) { 
+		long long sValue = std::stoll( s, NULL, 2 );
+		doubleBinary temp; 
+		temp.iValue = sValue;
+		return temp.dValue;
+	}
+
+}
 
 struct Point { 
 	int x, y;
@@ -64,12 +79,6 @@ std::ostream &operator<<( std::ostream &out, const Coefficient &c ) {
 
 std::vector<Point> trainPoints( DOTS_COUNT );
 std::vector<Coefficient> coefficients( POPULATION );
-
-std::string doubleToBinary( double in ) { 
-	doubleBinary temp; 
-	temp.dValue = in;
-	return utilities::decimalToBinaryString( temp.iValue );
-}
 
 void readPoints() {
 	std::ifstream x_fin( "x_train.csv" );
@@ -113,33 +122,85 @@ void printLosses() {
 	std::cout << std::endl;
 }
 
+void printPopulation() { 
+	for ( unsigned int i = 0; i < coefficients.size(); i++ ) 
+		std::cout << coefficients.at( i ) << "  ";
+	std::cout << std::endl;
+}
+
 int compareLoss( const Coefficient &c1, const Coefficient &c2 ) { 
 	double cost1 = costFunction( c1 );
 	double cost2 = costFunction( c2 );
 	return cost1 < cost2;
 }
 
-void sortPopulation() { 
+std::vector<double> crossOver( double a1, double a2, int slicePosition ) { 
+	std::cout << "a1, a2: " << a1 << ' ' << a2 << std::endl;
+	std::string a1String = utilities::doubleToBinaryString( a1 );
+	std::string a2String = utilities::doubleToBinaryString( a2 );
+	std::vector<double> returnValue( 2 );
+	std::cout << "Slice Position: " << slicePosition << std::endl;
+	std::cout << a1String << "        " << a2String << std::endl;
+	std::cout << a1String.substr( 0, slicePosition ) + a2String.substr( slicePosition, std::string::npos ) << "        " << a2String.substr( 0, slicePosition ) + a1String.substr( slicePosition, std::string::npos ) << std::endl;
+	returnValue[0] = utilities::binaryStringToDouble( a1String.substr( 0, slicePosition ) + a2String.substr( slicePosition, std::string::npos ) );
+	returnValue[1] = utilities::binaryStringToDouble( a2String.substr( 0, slicePosition ) + a1String.substr( slicePosition, std::string::npos ) );
+	return returnValue;
+}
 
-	sort( coefficients.begin(), coefficients.end(), compareLoss );
+std::vector<Coefficient> crossOver( const Coefficient &c1, const Coefficient &c2 ) { 
+
+	Coefficient child1, child2;
+	int slicePosition = rand() % 63 + 1;
+	auto temp = crossOver( c1.a, c2.a, slicePosition );
+	child1.a = temp[0]; child2.a = temp[1];
+
+	slicePosition = rand() % 63 + 1;
+	temp = crossOver( c1.b, c2.b, slicePosition );
+	child1.b = temp[0]; child2.b = temp[1];
+
+	slicePosition = rand() % 63 + 1;
+	temp = crossOver( c1.c, c2.c, slicePosition );
+	child1.c = temp[0], child2.c = temp[1];
+
+	std::vector<Coefficient> returnValue( 2 );
+	returnValue[1] = child1; returnValue[2] = child2;
+	return returnValue;
 
 }
 
+void crossOver() { 
+
+	size_t initialPopulation = coefficients.size();
+	for ( unsigned int i = 0; i < initialPopulation; i += 2 ) { 
+		auto childsTemp = crossOver( coefficients[i], coefficients[i+1] );
+		coefficients.insert( coefficients.end(), childsTemp.begin(), childsTemp.end() );
+		std::cout << "Pop size: " << coefficients.size() << std::endl;
+	}
+
+}
+
+void sortPopulation() { 
+	sort( coefficients.begin(), coefficients.end(), compareLoss );
+}
+
 int main () {
+
+/*	std::cout << utilities::decimalToBinaryString( 4611484187377963200 ) << std::endl;
+	exit( 0 );*/
 
 	srand( time( NULL ) );
 
 	readPoints();
 
 	generateFirstPopulation();
+//	printPopulation();
 /*	printLosses();
 	exit( 0 );*/
 	sortPopulation();
 
 	for ( int i = 0; i < GENERATION_COUNT; i++ ) {
 
-//		crossOver();
-//		mutate();
+		crossOver();
 		sortPopulation();
 //		dropOff();
 
